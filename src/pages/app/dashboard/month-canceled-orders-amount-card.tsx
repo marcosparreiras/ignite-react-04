74dc930096057c0d9f2d4ba3b56f29aa998f0282
Marcos Parreiras <marcosalp1998@gmail.com>
2024-08-21 23:09:20 -0300

    feat: add dashboard charts components

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { DollarSign, MoveDownIcon, Trash, X } from "lucide-react";
+import { MoveDownIcon } from "lucide-react";
 
 export function MonthCanceledOrdersAmountCard() {
   return (
---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,67 @@
+import colors from "tailwindcss/colors";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ResponsiveContainer,
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Line,
+} from "recharts";
+
+const data = [
+  { date: "21/12", revenue: 1200 },
+  { date: "22/12", revenue: 1450 },
+  { date: "23/12", revenue: 2550 },
+  { date: "24/12", revenue: 980 },
+  { date: "25/12", revenue: 450 },
+  { date: "26/12", revenue: 1200 },
+  { date: "27/12", revenue: 1250 },
+];
+
+export function RevenueChat() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no periodo
+          </CardTitle>
+          <CardDescription>Receita diária no periodo</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              }
+            />
+            <CartesianGrid vertical={false} className="stroke-muted" />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet["500"]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}
