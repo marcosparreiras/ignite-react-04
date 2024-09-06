cffede7e8b6c2a6279059746ed4e8b69e92ac810
Marcos Parreiras <marcosalp1998@gmail.com>
2024-09-06 00:11:51 -0300

    test: add dashboard metrics e2e test

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("display month revenue", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+  expect(page.getByText("R$ 354.000,00", { exact: true })).toBeVisible();
+  expect(page.getByText("+12% em realação ao mês")).toBeVisible();
+});
+
+test("display month order amount metric", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+  expect(page.getByText("1.320", { exact: true })).toBeVisible();
+  expect(page.getByText("-4% em realação ao mês passado")).toBeVisible();
+});
+
+test("display day order amount metric", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+  expect(page.getByText("20", { exact: true })).toBeVisible();
+  expect(page.getByText("-5% em relação a ontem")).toBeVisible();
+});
+
+test("display month canceled order amount metric", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+  expect(page.getByText("45", { exact: true })).toBeVisible();
+  expect(page.getByText("+4% em realação ao mês passado")).toBeVisible();
+});
